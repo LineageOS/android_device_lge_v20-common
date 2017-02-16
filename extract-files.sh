b0461ba7e1732c08732c45a3538cdbee991add5f
Bruno Martins <bgcngm@gmail.com>
2017-02-16 11:40:30 +0000

    v20-common: Update extractor script to consider shared blobs
    Change-Id: I4d42469d2e00e190e8c34d3505cdc1fe6f31b1b7

--- extract-files.sh
+++ extract-files.sh
@@ -60,3 +60,6 @@ extract "$MY_DIR"/../$DEVICE/proprietary-files-qc.txt "$SRC"
 extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC"
 
 "$MY_DIR"/setup-makefiles.sh
+
+# Extract the shared proprietary files
+"$CM_ROOT"/device/qcom/common/extractors/extract-files.sh "$SRC" "msm8996" "graphics"
