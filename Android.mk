54dd8a98e867bac7aef904e840a81a80dbe6f1c8
Matt Filetto <matt.filetto@gmail.com>
2017-02-18 23:34:21 -0800

    v20: inherit msm8996-common
    Change-Id: Id196cdcff2dbc45d5ff788706d523c5febe221f5
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
 #
 # Copyright 2016 The CyanogenMod Project
+# Copyright 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,87 +15,10 @@
 # limitations under the License.
 #
 
-ifneq ($(filter v20 h918 h910,$(TARGET_DEVICE)),)
+ifneq ($(filter v20 h918 h910 vs995 us996 ls997,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
-
-# CPPF Images
-CPPF_IMAGES := \
-    cppf.b00 cppf.b01 cppf.b02 cppf.b03 cppf.b04 \
-    cppf.b05 cppf.b06 cppf.mdt
-
-CPPF_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(CPPF_IMAGES)))
-$(CPPF_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "CPPF firmware link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /persist-lg/firmware/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(CPPF_SYMLINKS)
-# END CPPF Images
-
-# DXHDCP2 Images
-DXHDCP2_IMAGES := \
-    dxhdcp2.b00 dxhdcp2.b01 dxhdcp2.b02 dxhdcp2.b03 dxhdcp2.b04 \
-    dxhdcp2.b05 dxhdcp2.b06 dxhdcp2.mdt
-
-DXHDCP2_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(DXHDCP2_IMAGES)))
-$(DXHDCP2_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "DXHDCP2 firmware link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /firmware/image/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(DXHDCP2_SYMLINKS)
-# END DXHDCP2 Images
-
-# HASHSTORE Images
-HASHSTORE_IMAGES := \
-    hashstor.b00 hashstor.b01 hashstor.b02 hashstor.b03 hashstor.b04 \
-    hashstor.b05 hashstor.b06 hashstor.mdt
-
-HASHSTORE_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(HASHSTORE_IMAGES)))
-$(HASHSTORE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "HASHSTORE firmware link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /firmware/image/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(HASHSTORE_SYMLINKS)
-# END HASHSTORE Images
-
-# SECUREKS Images
-SECUREKS_IMAGES := \
-    secureks.b00 secureks.b01 secureks.b02 secureks.b03 secureks.b04 \
-    secureks.b05 secureks.b06 secureks.mdt
-
-SECUREKS_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(SECUREKS_IMAGES)))
-$(SECUREKS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "SECUREKS firmware link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /firmware/image/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(SECUREKS_SYMLINKS)
-# END SECUREKS Images
-
-# WIDEVINE Images
-WIDEVINE_IMAGES := \
-    widevine.b00 widevine.b01 widevine.b02 widevine.b03 widevine.b04 \
-    widevine.b05 widevine.b06 widevine.mdt
-
-WIDEVINE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(WIDEVINE_IMAGES)))
-$(WIDEVINE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "WIDEVINE firmware link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /persist-lg/firmware/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(WIDEVINE_SYMLINKS)
-# END WIDEVINE Images
-
-include device/lge/v20-common/tftp.mk
 
 endif
