8327dcf66c0b7c338bca6e9eb4459ca92ba9b3c0
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-01 03:59:09 -0400

    v20: Split off radio configs per-variant
    Change-Id: Ic4f6f60f27ff1ee05fe00bdf9605c179894cd155

---
+++ rootdir/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+# Device init scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.radio.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.radio.sh
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
