47c28eab54528cc174c3a37a5a5a7b19db15367b
Eric Meddaugh <eric@meddaughs.com>
2018-04-10 11:15:52 -0400

    v20: Switch to fingerprint@2.0-service
    Change-Id: Ida12ab525308a88bd38c25e5ddd438dd968af55f

--- v20.mk
+++ v20.mk
@@ -38,7 +38,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service
 
 # IR
 PRODUCT_PACKAGES += \
