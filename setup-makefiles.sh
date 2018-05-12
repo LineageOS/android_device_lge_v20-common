d33631498a350fcba3c12e897340243662d39bef
Elliott Mitchell <ehem+android_device_lge_h990@m5p.com>
2018-05-12 13:18:08 -0700

    v20: Remove CM/cm references, switch to LINEAGE/lineage
    The location of the parent extract-files.sh script is important, most of
    the rest is minor bookkeeping.  This has been updated for LineageOS in
    2017-2018, so note such in the Copyright header.
    
    Change-Id: I810004a3dd51eb6a09a95e8b0454484536e8cbfa

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,9 +22,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -31,7 +32,7 @@ fi
 . "$HELPER"
 
 # Initialize the helper for common device
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$CM_ROOT" true
+setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 
 # Copyright headers and common guards
 write_headers "v20 h910 h918 us996 vs995 ls997"
@@ -72,7 +73,7 @@ printf '\n%s\n' "\$(call inherit-product, vendor/qcom/binaries/msm8996/graphics/
 write_footers
 
 # Reinitialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 
 # Copyright headers and guards
 write_headers
